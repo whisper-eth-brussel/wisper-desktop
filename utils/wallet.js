e60b428a111a6d95663943e81e074684a6979dd9
necipsagiro <sagiroglun22@itu.edu.tr>
2024-07-13 20:02:37 +0200

    Refactor wallet.js and update signTx function

--- utils/wallet.js
+++ utils/wallet.js
@@ -1,12 +1,59 @@
-// sign tx
-// generate key pair if not exists
-// hash
+const crypto = require('crypto');
+const { safeStorage } = require('electron');
+const fs = require('fs');
+const path = require('path');
+const { app, safeStorage } = require('electron');
 
-function signTx() {
+const privateKeyPath = path.join(app.getPath('userData'), 'encryptedPrivateKey');
+const publicKeyPath = path.join(app.getPath('userData'), 'publicKey');
 
-  // hash falan da yap
+function signTx(tx, callback) {
+  craeteOrGetEncrpytedPrivateKey((err, privateKey) => {
+    if (err)
+      return callback(err);
 
-  return signedTransaction; // string
+    crypto.sign('sha256', {
+      data: tx.data
+    }, privateKey, (err, signature) => {
+      if (err)
+          return callback(err);
+
+      tx.hash = crypto.createHash('sha256').update(tx.data).digest('hex');
+      tx.signature = signature;
+
+      return callback(null, tx);
+    });
+  });
+};
+
+function craeteOrGetEncrpytedPrivateKey(callback) {
+  if (!fs.existsSync(privateKeyPath)) {
+    crypto.generateKeyPair('rsa', {
+      modulusLength: 4096
+    }, (err, publicKey, privateKey) => {
+      if (err)
+        return callback(err);
+
+      fs.writeFile(privateKeyPath, safeStorage.encryptString(privateKey), 'utf8', err => {
+        if (err)
+          return callback(err);
+
+        fs.writeFile(publicKeyPath, publicKey, 'utf8', err => {
+          if (err)
+            return callback(err);
+
+          return callback(null, privateKey);
+        });
+      });
+    });
+  } else {
+    fs.readFile(privateKeyPath, 'utf8', err => {
+      if (err)
+        return callback(err);
+
+      return callback(null, safeStorage.decryptString(data));
+    });
+  };
 };
 
 module.exports = {
