592b5b32a204e6d60766a6a6cb936ecc6dc0a4c8
necipsagiro <sagiroglun22@itu.edu.tr>
2024-07-14 02:59:07 +0200

    chore: Add roomRouteController to handle room-related routes

--- controllers/room/join/get.js
+++ controllers/room/join/get.js
@@ -1,11 +1,6 @@
-// const id = Buffer.from(os.networkInterfaces()['en0'][0].address, 'utf8').toString('hex');
-
-
 module.exports = (req, res) => {
-  if (!req.query.id || !req.query.id.trim())
-    return res.json({ err: 'Invalid room id' });
+  if (!req.query.id || typeof req.query.id != "string")
+    return res.status(400).json({ error: "Bad request" });
 
-  const peerIpAddress = Buffer.from(req.query.id.trim(), 'hex').toString('utf8');
-
-
+  return res.redirect(`/room/join/${req.query.id}`);
 };
--- routes/peerRoute.js
+++ routes/peerRoute.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 
-const peerIntroducePostController = require("../controllers/peer/introduce/post");
+const peerIntroducePostController = require("../controllers/peer/introduce/get");
 const peerRecognizePostController = require("../controllers/peer/recognize/post");
 
-router.post("/introduce/:destinationIp", peerIntroducePostController);
-
-router.post("/recognize", peerRecognizePostController);
+router.post(
+  "/introduce/:destinationIp",
+  peerIntroducePostController
+);
+router.post(
+  "/recognize",
+  peerRecognizePostController
+);
 
 module.exports = router;
